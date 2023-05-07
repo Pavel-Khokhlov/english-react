263e0c68c5a6550effed6c25beef9f1d3e7f5f0b
MacbookPro <macbook@Pavel-Khokhlov.local>
2023-05-07 12:42:30 +1000

    feat: get visitor and version info

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Achive from "../Pages/Achive/Achive";
 import Contacts from "../Pages/Contacts/Contacts";
@@ -12,7 +12,45 @@ import "./App.sass";
 import Feedbacks from "../Pages/Feedbacks/Feedbacks";
 import PopupNotification from "../Modals/Notification/PopupNotification";
 
+const VisitorAPI = require("visitorapi");
+
+interface VisitorAPIProps {
+  ipAddress: string;
+  countryCode: string;
+  countryName: string;
+  region: string;
+  city: string;
+  cityLatLong: string;
+  currencies: string;
+  languages: string;
+  browser: string;
+  browserVersion: string;
+  deviceBrand: string;
+  deviceModel: string;
+  deviceFamily: string;
+  os: string;
+  osVersion: string;
+}
+
 function App() {
+  const [visitorData, setVisitorData] = useState({}); // store the whole json
+  const Visitor_Data = sessionStorage.getItem('visitor_data');
+  useEffect(() => {
+    if (!Visitor_Data) {
+      VisitorAPI(
+        "R9nfBw2jsCXvbv4BWDXd",
+        (data: VisitorAPIProps) => {
+          setVisitorData(data);
+          sessionStorage.setItem('visitor_data', JSON.stringify(data));
+        }
+      );
+    } else {
+      setVisitorData(JSON.parse(Visitor_Data));
+    }
+  },[Visitor_Data]);
+
+  console.log(visitorData);
+
   return (
     <RecoilRoot>
       <Header />
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Navigation from "../Navigation/Navigation";
 
+import app_json from "../../../package.json";
+
 import "./Footer.sass";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
+  console.log(`APP_VERSION`, app_json.version);
   return (
     <footer className="footer">
       <section className="footer__body">
         <Navigation location="footer" />
-        <p className="footer__paragraph">@Created by Business Web Studio</p>
-        <p className="footer__paragraph">{process.env.REACT_APP_YEAR_START} - {currentYear}</p>
+        <div className="footer__info">
+          <p className="footer__paragraph">
+            &#9400; {process.env.REACT_APP_YEAR_START} - {currentYear} Created by Business Web Studio
+          </p>
+          <p className="footer__paragraph">
+            Version: {app_json.version}
+          </p>
+        </div>
       </section>
     </footer>
   );
